93937918f6f494d94dc2d405ec0aa389430eb040
nick909 <nicholas.silva909@outlook.com>
2022-12-22 14:56:50 -0300

    up

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -4,7 +4,7 @@ const Cabecalho = () => {
     return (
         <header className="cabecalho">
             <div className="imagem-logo" role="img" aria-label='Logo do Sorteador'></div>
-            <img className='participante' src="/imagens/participante.png" alt="Participante com um presente na mão" />
+            <img className='participante' src="/images/participante.png" alt="Participante com um presente na mão" />
         </header>
     )
 }
